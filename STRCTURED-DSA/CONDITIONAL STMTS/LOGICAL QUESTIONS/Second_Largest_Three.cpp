857f846d597f4dfe2f38ffb213e9197b95d442a0
Umesh Jadhav <umeshrajput24196@gmail.com>
2026-09-10 07:18:02 +0530

    cp4.1.1

---
+++ STRCTURED-DSA/CONDITIONAL STMTS/LOGICAL QUESTIONS/Second_Largest_Three.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+
+    cout << "Enter three different numbers: ";
+    cin >> a >> b >> c;
+
+    if ((a > b && a < c) || (a < b && a > c)) {
+        cout << "Second Largest = " << a;
+    }
+    else if ((b > a && b < c) || (b < a && b > c)) {
+        cout << "Second Largest = " << b;
+
+    }
+    else {
+        cout << "Second Largest = " << c;
+    }
+
+    return 0;
+}
+
+
