be3d6e17771a96d43a27ad11e0d7576dd3291763
Umesh Jadhav <umeshrajput24196@gmail.com>
2026-09-18 20:10:21 +0530

    cp4.1.1

--- STRCTURED-DSA/CONDITIONAL STMTS/LOGICAL QUESTIONS/Second_Largest_Three.cpp
+++ STRCTURED-DSA/CONDITIONAL STMTS/LOGICAL QUESTIONS/Second_Largest_Three.cpp
@@ -11,3 +11,16 @@ int main() {
         cout << "Second Largest = " << a;
     }
     else if ((b > a && b < c) || (b < a && b > c)) {
+        cout << "Second Largest = " << b;
+
+    }
+    else {
+        cout << "Second Largest = " << c;
+
+   }
+    return 0;
+}
+
+
+
+
